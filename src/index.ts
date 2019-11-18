dddd4f2e2adb7b436ef3949f456cdbf43c5498e4
vasyas <vasyl@stashuk.com>
2019-11-18 18:00:09 +0200

    ocpp-compat mesages

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export {createRpcClient} from "./remote"
 export {createRpcServer, LocalTopicImpl} from "./local"
 export {setLogger} from "./logger"
-export {Topic} from "./rpc"
+export {Topic, RpcContext} from "./rpc"
