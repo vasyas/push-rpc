27a1243c457bf2b2cb578337ccf7ba004ce599b5
vasyas <vasyl@stashuk.com>
2019-12-12 12:10:11 +0200

    tcp protocol

---
+++ tcp/src/client.ts
@@ -0,0 +1,36 @@
+import {Socket} from "@push-rpc/core"
+import * as net from "net"
+
+export function createSocket(host, port): Socket {
+  const socket = new net.Socket()
+  socket.connect(port, host)
+
+  return wrapSocket(socket)
+}
+
+export function wrapSocket(socket: net.Socket): Socket {
+  return {
+    onMessage: h => {
+      socket.on("data", data => h(data.toString("utf8")))
+    },
+
+    onOpen: h => socket.on("connect", h),
+
+    onClose: h =>
+      socket.on("close", error => {
+        h(error ? 1 : 0, error ? "error" : null)
+      }),
+
+    onError: h => socket.on("error", h),
+
+    onPong: h => {
+      // not implemented
+    },
+
+    terminate: () => socket.destroy(),
+    send: data => socket.write(data),
+    ping: data => {
+      // not implemented
+    },
+  }
+}
---
+++ tcp/src/server.ts
@@ -0,0 +1,21 @@
+import * as net from "net"
+import {SocketServer} from "@push-rpc/core"
+import {wrapSocket} from "./client"
+
+export function createSocketServer(port): SocketServer {
+  const s = new net.Server()
+  s.listen(port)
+
+  return {
+    onError: e => s.on("error", e),
+
+    onConnection: h => {
+      s.on("connection", (socket, req) => {
+        h(wrapSocket(socket), req)
+      })
+    },
+    close: h => {
+      s.close(h)
+    },
+  }
+}
