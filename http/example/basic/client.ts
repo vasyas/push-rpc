b50a5853f524350b5a24a1748bfbb74756fc2b97
vasyas <vasyl@stashuk.com>
2020-01-19 23:17:13 +0200

    http: handle client hang

--- http/example/basic/client.ts
+++ http/example/basic/client.ts
@@ -1,16 +1,19 @@
+import * as log from "why-is-node-running"
 import "isomorphic-fetch"
-import {createRpcClient, setLogger} from "@push-rpc/core"
+import {createRpcClient, setLogger} from "../../../core/src"
 import {createHttpClient} from "../../src/client"
 
 setLogger(console)
 ;(async () => {
-  const {remote} = await createRpcClient(0, () => createHttpClient("http://localhost:5555/rpc"))
-
   try {
+    const {remote} = await createRpcClient(0, () => createHttpClient("http://localhost:5555/rpc"))
+
     console.log("Client connected")
 
     console.log("From server: " + (await remote.getHello()))
   } catch (e) {
     console.log(e)
   }
+
+  // log()
 })()
