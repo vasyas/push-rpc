a3bd0e1f5e7a36b0d7a761b86b41d2109129d443
vasyas <vasyl@stashuk.com>
2024-01-23 20:19:05 +0100

    WIP

--- tests/connection.ts
+++ tests/connection.ts
@@ -1,12 +1,11 @@
 import {assert} from "chai"
-import {createTestClient, startTestServer} from "./testUtils.js"
+import {createTestClient, startTestServer, testServer} from "./testUtils.js"
 
-// should be implemented in the end
 describe("connection", () => {
   it("server close connection on keep alive timeout", async () => {
-    const keepAliveTimeout = 1000
+    const keepAliveTimeout = 300
 
-    const rpcServer = await startTestServer(
+    const services = await startTestServer(
       {
         test: {
           async call() {},
@@ -17,16 +16,16 @@ describe("connection", () => {
       }
     )
 
-    const client = await createTestClient()
+    const remote = await createTestClient<typeof services>()
 
-    await client.test.call()
-    assert.equal(rpcServer.getConnectedIds().length, 1)
+    await remote.test.call.subscribe(() => {})
+    assert.equal(testServer?._subscriptions().size, 1)
 
     // wait for timeout
     await new Promise(r => setTimeout(r, keepAliveTimeout * 2.5))
 
     // should be closed
-    assert.equal(rpcServer.getConnectedIds().length, 0)
+    assert.equal(testServer?._subscriptions().size, 0)
   }).timeout(5000)
 
   /*
