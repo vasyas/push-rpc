eb034adf070299c6143a24b4d5d74418ad07792c
vasyas <vasyl@stashuk.com>
2024-01-24 16:34:24 +0100

    WIP

--- tests/context.ts
+++ tests/context.ts
@@ -1,5 +1,7 @@
 import {createTestClient, startTestServer} from "./testUtils.js"
 import {assert} from "chai"
+import {IncomingMessage} from "http"
+import {RpcContext} from "../src/server/index.js"
 
 describe("context", () => {
   it("available in call", async () => {
@@ -21,7 +23,32 @@ describe("context", () => {
     assert.ok(ctx!.clientId)
   })
 
-  it("override creation", async () => {})
+  it("override creation", async () => {
+    let ctx = null
+
+    const services = await startTestServer(
+      {
+        test: {
+          async call(passedCtx?: any) {
+            ctx = passedCtx
+          },
+        },
+      },
+      {
+        createContext(): Promise<RpcContext> {
+          return Promise.resolve({clientId: "test", newKey: "bla"})
+        },
+      }
+    )
+
+    const client = await createTestClient<typeof services>()
+
+    await client.test.call()
+
+    assert.ok(ctx)
+    assert.equal(ctx!.clientId, "test")
+    assert.equal(ctx!.newKey, "bla")
+  })
 
   it("available in subscribe", async () => {})
   it("available in trigger", async () => {})
