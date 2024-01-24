f9594178acbb56cc4f65618ffd3155abd02ddc47
vasyas <vasyl@stashuk.com>
2024-01-24 16:19:13 +0100

    RFT

---
+++ src/server/RpcServerImpl.ts
@@ -0,0 +1,165 @@
+import {PublishServicesOptions, RpcServer} from "./index.js"
+import {LocalSubscriptions} from "./LocalSubscriptions.js"
+import http from "http"
+import {ConnectionsServer} from "./ConnectionsServer.js"
+import {PromiseCache} from "../utils/promises.js"
+import {serveHttpRequest} from "./http.js"
+import {RemoteFunction, RpcError, RpcErrors, Services} from "../rpc.js"
+import {log} from "../logger.js"
+import {withMiddlewares} from "../utils/middleware.js"
+import {ServicesWithTriggers, withTriggers} from "./local.js"
+
+export class RpcServerImpl<S extends Services> implements RpcServer {
+  constructor(
+    private readonly services: S,
+    private readonly options: PublishServicesOptions
+  ) {
+    this.connectionsServer = new ConnectionsServer(
+      this.httpServer,
+      {pingInterval: options.pingInterval},
+      (clientId) => {
+        this.localSubscriptions.unsubscribeAll(clientId)
+      }
+    )
+
+    this.httpServer.addListener("request", (req, res) =>
+      serveHttpRequest(req, res, options.path, {
+        call: this.call,
+        subscribe: this.subscribe,
+        unsubscribe: this.unsubscribe,
+      })
+    )
+  }
+
+  start() {
+    return new Promise<void>((resolve, reject) => {
+      this.httpServer.on("error", (err) => {
+        reject(err)
+      })
+
+      this.httpServer.listen(this.options.port, this.options.host, () => {
+        resolve()
+      })
+    })
+  }
+
+  async close() {
+    await this.connectionsServer.close()
+    await new Promise<void>((resolve, reject) => {
+      this.httpServer.closeIdleConnections()
+      this.httpServer.close((err) => {
+        if (err) reject(err)
+        else resolve(err)
+      })
+    })
+  }
+
+  createServicesWithTriggers(): ServicesWithTriggers<S> {
+    return withTriggers(this.localSubscriptions, this.services)
+  }
+
+  _allSubscriptions() {
+    return this.localSubscriptions._allSubscriptions()
+  }
+
+  private readonly localSubscriptions = new LocalSubscriptions()
+  private readonly invocationCache = new PromiseCache()
+  private readonly connectionsServer: ConnectionsServer
+  private readonly httpServer = http.createServer()
+
+  private call = async (
+    clientId: string,
+    itemName: string,
+    parameters: unknown[]
+  ): Promise<unknown> => {
+    const item = this.getItem(itemName)
+
+    if (!item) {
+      throw new RpcError(RpcErrors.NotFound, `Item ${itemName} not found`)
+    }
+
+    try {
+      return await this.invokeItem(clientId, itemName, item, parameters)
+    } catch (e) {
+      log.error(`Cannot call item ${itemName}.`, e)
+      throw e
+    }
+  }
+
+  private subscribe = async (clientId: string, itemName: string, parameters: unknown[]) => {
+    const item = this.getItem(itemName)
+
+    if (!item) {
+      throw new RpcError(RpcErrors.NotFound, `Item ${itemName} not found`)
+    }
+
+    try {
+      const data = await this.invokeItem(clientId, itemName, item, parameters)
+
+      this.localSubscriptions.subscribe(clientId, itemName, parameters, async () => {
+        try {
+          const data = await this.invokeItem(clientId, itemName, item, parameters)
+
+          // TODO do not send if data is the same
+
+          this.connectionsServer.publish(clientId, itemName, parameters, data)
+        } catch (e) {
+          log.error("Cannot get data for subscription", e)
+        }
+      })
+
+      return data
+    } catch (e) {
+      log.error(`Failed to subscribe ${itemName}`, e)
+      throw e
+    }
+  }
+
+  private unsubscribe = async (clientId: string, itemName: string, parameters: unknown[]) => {
+    try {
+      this.localSubscriptions.unsubscribe(clientId, itemName, parameters)
+    } catch (e) {
+      log.error(`Failed to unsubscribe ${itemName}`, e)
+      throw e
+    }
+  }
+
+  private getItem(
+    itemName: string,
+    root: any = this.services
+  ): {function: RemoteFunction; container: any} | undefined {
+    const parts = itemName.split("/")
+
+    let item = root
+    let parent
+
+    for (const part of parts) {
+      if (!item) return undefined
+
+      parent = item
+      item = item[part]
+    }
+
+    if (!item) return undefined
+
+    return {
+      function: item,
+      container: parent,
+    }
+  }
+
+  private invokeItem(
+    clientId: string,
+    itemName: string,
+    item: {function: RemoteFunction; container: any},
+    parameters: unknown[]
+  ): Promise<unknown> {
+    return this.invocationCache.invoke({clientId, itemName, parameters}, () => {
+      const invokeItem = (...params: unknown[]) => {
+        return item.function.call(item.container, ...params)
+      }
+
+      return withMiddlewares(this.options.middleware, invokeItem, ...parameters, {})
+    })
+  }
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,11 @@
-import {RemoteFunction, RpcError, RpcErrors, Services} from "../rpc.js"
-import {LocalSubscriptions} from "./LocalSubscriptions.js"
-import {ServicesWithTriggers, withTriggers} from "./local.js"
-import http from "http"
-import {serveHttpRequest} from "./http.js"
-import {Middleware, withMiddlewares} from "../utils/middleware.js"
-import {log} from "../logger.js"
-import {ConnectionsServer} from "./ConnectionsServer.js"
-import {PromiseCache} from "../utils/promises.js"
+import {Services} from "../rpc.js"
+import {ServicesWithTriggers} from "./local.js"
+import {Middleware} from "../utils/middleware.js"
+import {RpcServerImpl} from "./RpcServerImpl.js"
 
-export function publishServices<S extends Services>(
+export async function publishServices<S extends Services>(
   services: S,
-  overrideOptions: Partial<PublishServicesOptions> = {}
+  overrideOptions: Partial<PublishServicesOptions> & {port: number}
 ): Promise<{
   server: RpcServer
   services: ServicesWithTriggers<S>
@@ -20,134 +15,18 @@ export function publishServices<S extends Services>(
     ...overrideOptions,
   }
 
-  const localSubscriptions = new LocalSubscriptions()
+  const rpcServer = new RpcServerImpl<S>(services, options)
 
-  const httpServer = http.createServer()
+  await rpcServer.start()
 
-  const connectionsServer = new ConnectionsServer(
-    httpServer,
-    {pingInterval: options.pingInterval},
-    (clientId) => {
-      localSubscriptions.unsubscribeAll(clientId)
-    }
-  )
-
-  const invocationCache = new PromiseCache()
-
-  httpServer.addListener("request", (req, res) =>
-    serveHttpRequest(req, res, options.path, {
-      async call(clientId: string, itemName: string, parameters: unknown[]): Promise<unknown> {
-        const item = getItem(services, itemName)
-
-        if (!item) {
-          throw new RpcError(RpcErrors.NotFound, `Item ${itemName} not found`)
-        }
-
-        try {
-          return await invokeItem(
-            invocationCache,
-            clientId,
-            itemName,
-            item,
-            parameters,
-            options.middleware
-          )
-        } catch (e) {
-          log.error(`Cannot call item ${itemName}.`, e)
-          throw e
-        }
-      },
-
-      async subscribe(clientId: string, itemName: string, parameters: unknown[]) {
-        const item = getItem(services, itemName)
-
-        if (!item) {
-          throw new RpcError(RpcErrors.NotFound, `Item ${itemName} not found`)
-        }
-
-        try {
-          const data = await invokeItem(
-            invocationCache,
-            clientId,
-            itemName,
-            item,
-            parameters,
-            options.middleware
-          )
-
-          localSubscriptions.subscribe(clientId, itemName, parameters, async () => {
-            try {
-              const data = await invokeItem(
-                invocationCache,
-                clientId,
-                itemName,
-                item,
-                parameters,
-                options.middleware
-              )
-
-              // TODO do not send if data is the same
-
-              connectionsServer.publish(clientId, itemName, parameters, data)
-            } catch (e) {
-              log.error("Cannot get data for subscription", e)
-            }
-          })
-
-          return data
-        } catch (e) {
-          log.error(`Failed to subscribe ${itemName}`, e)
-          throw e
-        }
-      },
-
-      async unsubscribe(clientId: string, itemName: string, parameters: unknown[]) {
-        try {
-          localSubscriptions.unsubscribe(clientId, itemName, parameters)
-        } catch (e) {
-          log.error(`Failed to unsubscribe ${itemName}`, e)
-          throw e
-        }
-      },
-    })
-  )
-
-  function closeHttpServer() {
-    return new Promise<void>((resolve, reject) => {
-      httpServer.closeIdleConnections()
-      httpServer.close((err) => {
-        if (err) reject(err)
-        else resolve(err)
-      })
-    })
+  return {
+    services: rpcServer.createServicesWithTriggers(),
+    server: rpcServer,
   }
-
-  return new Promise((resolve, reject) => {
-    httpServer.on("error", (err) => {
-      reject(err)
-    })
-
-    httpServer.listen(options.port, options.host, () => {
-      resolve({
-        services: withTriggers(localSubscriptions, services),
-        server: {
-          async close() {
-            await connectionsServer.close()
-            await closeHttpServer()
-          },
-
-          _allSubscriptions() {
-            return localSubscriptions._allSubscriptions()
-          },
-        },
-      })
-    })
-  })
 }
 
 export type RpcServer = {
   close(): Promise<void>
-
   // test-only
   _allSubscriptions(): Array<any[]>
 }
@@ -162,47 +41,6 @@ export type PublishServicesOptions = {
 
 export type RpcContext = {
   clientId: string
-}
-
-function getItem(
-  root: any,
-  itemName: string
-): {function: RemoteFunction; container: any} | undefined {
-  const parts = itemName.split("/")
-
-  let item = root
-  let parent
-
-  for (const part of parts) {
-    if (!item) return undefined
-
-    parent = item
-    item = item[part]
-  }
-
-  if (!item) return undefined
-
-  return {
-    function: item,
-    container: parent,
-  }
-}
-
-function invokeItem(
-  invocationCache: PromiseCache,
-  clientId: string,
-  itemName: string,
-  item: {function: RemoteFunction; container: any},
-  parameters: unknown[],
-  middlewares: Middleware[]
-): Promise<unknown> {
-  return invocationCache.invoke({clientId, itemName, parameters}, () => {
-    const invokeItem = (...params: unknown[]) => {
-      return item.function.call(item.container, ...params)
-    }
-
-    return withMiddlewares(middlewares, invokeItem, ...parameters, {})
-  })
 }
 
 const defaultOptions: Omit<PublishServicesOptions, "port"> = {
