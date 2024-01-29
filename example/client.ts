606d0818ebcc9f175955d94bc1447b9b87886128
vasyas <vasyl@stashuk.com>
2024-01-29 19:22:22 +0100

    WIP

--- example/client.ts
+++ example/client.ts
@@ -1,12 +1,16 @@
 import {Services} from "./api"
 import {consumeServices} from "../src/client/index"
 
-const {remote} = await consumeServices<Services>("http://127.0.0.1:8080/rpc")
+async function startClient() {
+  const {remote} = await consumeServices<Services>("http://127.0.0.1:8080/rpc")
 
-console.log("Client created")
+  console.log("Client created")
 
-await remote.todo.getTodos.subscribe((todos) => {
-  console.log("Got todo items", todos)
-}, null)
+  await remote.todo.getTodos.subscribe((todos) => {
+    console.log("Got todo items", todos)
+  }, null)
 
-await remote.todo.addTodo({text: "Buy groceries"})
+  await remote.todo.addTodo({text: "Buy groceries"})
+}
+
+startClient()
--- example/server.ts
+++ example/server.ts
@@ -4,33 +4,37 @@ import {RemoteFunction} from "../src/rpc"
 
 const storage: Todo[] = []
 
-class TodoServiceImpl implements TodoService {
-  async addTodo({text}: {text: string}) {
-    storage.push({
-      id: "" + Math.random(),
-      text,
-      status: "open",
-    })
+async function startServer() {
+  class TodoServiceImpl implements TodoService {
+    async addTodo({text}: {text: string}) {
+      storage.push({
+        id: "" + Math.random(),
+        text,
+        status: "open",
+      })
 
-    console.log("New todo item added")
-    services.todo.getTodos.trigger()
+      console.log("New todo item added")
+      services.todo.getTodos.trigger()
+    }
+
+    async getTodos() {
+      return storage
+    }
+
+    [key: string]: RemoteFunction // TODO get rid of this
   }
 
-  async getTodos() {
-    return storage
-  }
+  const {services} = await publishServices(
+    {
+      todo: new TodoServiceImpl(),
+    },
+    {
+      port: 8080,
+      path: "/rpc",
+    }
+  )
 
-  [key: string]: RemoteFunction // TODO is there a way to skip this?
+  console.log("RPC Server started at http://localhost:8080/rpc")
 }
 
-const {services} = await publishServices(
-  {
-    todo: new TodoServiceImpl(),
-  },
-  {
-    port: 8080,
-    path: "/rpc",
-  }
-)
-
-console.log("RPC Server started at http://localhost:8080/rpc")
+startServer()
