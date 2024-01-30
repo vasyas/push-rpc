ec346923dfc6964d2a0d933c695e37765dc3425e
vasyas <vasyl@stashuk.com>
2024-01-30 18:47:03 +0100

    Build WIP

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,18 @@
-export {
-  RemoteFunction,
-  Services,
-  Consumer,
-  RpcError,
-  RpcErrors,
-  CallOptions,
-  RpcContext,
-  RpcConnectionContext,
-} from "./rpc.js"
+export type {RemoteFunction, Services, Consumer, RpcContext, RpcConnectionContext} from "./rpc.js"
+export {RpcError, RpcErrors, CallOptions} from "./rpc.js"
 
-export {Middleware, withMiddlewares} from "./utils/middleware.js"
+export type {Middleware} from "./utils/middleware.js"
+export {withMiddlewares} from "./utils/middleware.js"
 
-export {RpcServer, publishServices, PublishServicesOptions} from "./server/index.js"
+export type {RpcServer, PublishServicesOptions} from "./server/index.js"
+export {publishServices} from "./server/index.js"
 
-export {ServicesWithTriggers} from "./server/local.js"
+export type {ServicesWithTriggers} from "./server/local.js"
 
-export {RpcClient, consumeServices, ConsumeServicesOptions} from "./client/index.js"
+export type {RpcClient, ConsumeServicesOptions} from "./client/index.js"
+export {consumeServices} from "./client/index.js"
 
-export {ServicesWithSubscriptions} from "./client/remote.js"
+export type {ServicesWithSubscriptions} from "./client/remote.js"
 
 export {log, setLogger} from "./logger.js"
 export {safeStringify, safeParseJson} from "./utils/json.js"
