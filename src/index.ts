36774ffb8ad10fd49417f438f5e6397a3bd2a7ef
Vasyl Stashuk <vasyl@stashuk.com>
2025-05-19 18:15:49 +0200

    Export helper types

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,13 @@ export type {RpcServer, PublishServicesOptions} from "./server/index.js"
 export type {HttpServerHooks} from "./server/http.js"
 export {publishServices} from "./server/index.js"
 
-export type {ServicesWithTriggers} from "./server/local.js"
+export type {ServicesWithTriggers, ThrottleSettings} from "./server/local.js"
 
 export type {RpcClient, ConsumeServicesOptions, ClientCache} from "./client/index.js"
 export {consumeServices} from "./client/index.js"
 
-export type {ServicesWithSubscriptions} from "./client/remote.js"
+export type {ServicesWithSubscriptions, AddParameters} from "./client/remote.js"
 
 export {log, setLogger} from "./logger.js"
 export {safeStringify, safeParseJson} from "./utils/json.js"
+export {ExtractPromiseResult} from "./utils/types.js"
